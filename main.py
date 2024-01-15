35edc3c31dca94c29e54ba9bf30746fd260aa917
Manick Arumuganainar <manick.arumuganainar@yoppworks.com>
2024-01-14 23:27:03 -0500

    style: formatted with black

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import urllib.error
 import requests
 from typing import Tuple
 
+
 def load_env_variable():
     """
     Load env variables for local testing
     """
     try:
         from dotenv import load_dotenv
+
         load_dotenv()
     except ImportError:
         pass
+
 
 def load_data() -> dict:
     """
@@ -24,55 +27,58 @@ def load_data() -> dict:
         _type_: _description_
     """
     try:
-        file_name = os.getenv('YAML_FILE_NAME')
-        with open(file_name,"r") as y:
+        file_name = os.getenv("YAML_FILE_NAME")
+        with open(file_name, "r") as y:
             file_content = yaml.full_load(y)
     except FileNotFoundError:
-        print(f'File with name {file_name} not found')
+        print(f"File with name {file_name} not found")
         os._exit(1)
     return file_content
 
-def get_today_list(file_content:dict)-> Tuple[list,str]:
+
+def get_today_list(file_content: dict) -> Tuple[list, str]:
     """Reads the dict and return birthday and anniversary matching in list along with the date
 
     Returns:
         list: Matching entries for current date
         str: the Month and year e.g. January 14
     """
-    today_month,today_date = date.today().strftime('%B-%-d').split('-')
+    today_month, today_date = date.today().strftime("%B-%-d").split("-")
     collected_details = file_content.get(today_month).get(int(today_date))
-    return collected_details, f'{today_month}-{today_date}'
+    return collected_details, f"{today_month}-{today_date}"
 
-def notify_discord(today_date:str,message_list:list):
+
+def notify_discord(today_date: str, message_list: list):
     """Webhook call for Discord server
 
     Args:
         today_date (str): Today date to be sent in Discord message
-        message_list (list): List of entries matching 
+        message_list (list): List of entries matching
     """
-    discord_web_hook_url = os.getenv('DISCORD_WEBHOOK')
+    discord_web_hook_url = os.getenv("DISCORD_WEBHOOK")
     message_str = "\n".join(message_list)
-    headers = {'Content-Type': 'application/json'}
-    payload = {'content': f'Birthday & Anniversary reminder for {today_date} is {message_str}'}
+    headers = {"Content-Type": "application/json"}
+    payload = {"content": f"Birthday & Anniversary reminder for {today_date} is {message_str}"}
     try:
-        requests.post(discord_web_hook_url,headers=headers,json=payload)
+        requests.post(discord_web_hook_url, headers=headers, json=payload)
         print("Message posted to discord successfully")
     except requests.exceptions.RequestException as err:
-        print(f'Exception {err} receoved')
+        print(f"Exception {err} receoved")
 
 
 def main():
     """
     main program which is called on each run
     """
-    if os.getenv('MODE') == None or os.getenv('MODE') != "pipeline":
+    if os.getenv("MODE") == None or os.getenv("MODE") != "pipeline":
         load_env_variable()
     file_content = load_data()
-    collected_list,today_date = get_today_list(file_content)
+    collected_list, today_date = get_today_list(file_content)
     if collected_list:
-        notify_discord(today_date,collected_list)
+        notify_discord(today_date, collected_list)
     else:
         print("No birthday or reminder found for today")
+
 
 if __name__ == "__main__":
     main()
